8a40b45b21a159610530748f63f075606e893190
Vadym Starchenko <starvadim@ukr.net>
2022-02-10 18:50:07 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+   let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+   return color;
+}
+let intervalId = null;
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+
+function onStart() {
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    console.log(intervalId);
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+}
+
+function onStop() {
+    clearInterval(intervalId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+btnStart.addEventListener("click", onStart);
+btnStop.addEventListener("click", onStop);
